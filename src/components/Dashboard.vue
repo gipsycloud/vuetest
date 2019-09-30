a60ef6e2bc3bf3b44ecb4531199231430a2b368d
aungwin <aungthiwin@sys-city.com>
2019-09-30 16:35:24 +0630

    adding verity framework

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,161 @@
+<template>
+  <v-card
+    max-width="500"
+    class="mx-auto"
+  >
+    <v-toolbar
+      color="pink"
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Inbox</v-toolbar-title>
+
+      <div class="flex-grow-1"></div>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-checkbox-marked-circle</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-list two-line>
+      <v-list-item-group
+        v-model="selected"
+        multiple
+        active-class="pink--text"
+      >
+        <template v-for="(item, index) in items">
+          <v-list-item :key="item.title">
+            <template v-slot:default="{ active, toggle }">
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+                <v-list-item-subtitle class="text--primary" v-text="item.headline"></v-list-item-subtitle>
+                <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-list-item-action-text v-text="item.action"></v-list-item-action-text>
+                <v-icon
+                  v-if="!active"
+                  color="grey lighten-1"
+                >
+                  star_border
+                </v-icon>
+
+                <v-icon
+                  v-else
+                  color="yellow"
+                >
+                  star
+                </v-icon>
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+
+          <v-divider
+            v-if="index + 1 < items.length"
+            :key="index"
+          ></v-divider>
+        </template>
+      </v-list-item-group>
+      <div class="edit-btn">
+        <v-btn class="mx-2" fab dark color="indigo">
+          <v-icon dark>mdi-plus</v-icon>
+        </v-btn>
+      </div>
+    </v-list>
+    <div class="text-center">
+      <v-dialog
+      v-model="dialog"
+      width="500"
+      >
+      <template v-slot:activator="{ on }">
+        <v-btn
+        color="red lighten-2"
+        dark
+        v-on="on"
+        >
+        Click Me
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title
+      class="headline grey lighten-2"
+      primary-title
+      >
+      Privacy Policy
+    </v-card-title>
+
+    <v-card-text>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </v-card-text>
+
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <div class="flex-grow-1"></div>
+      <v-btn
+      color="primary"
+      text
+      @click="dialog = false"
+      >
+      I accept
+    </v-btn>
+  </v-card-actions>
+</v-card>
+</v-dialog>
+</div>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      selected: [2],
+      items: [
+        {
+          action: '15 min',
+          headline: 'Brunch this weekend?',
+          title: 'Ali Connors',
+          subtitle: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+        },
+        {
+          action: '2 hr',
+          headline: 'Summer BBQ',
+          title: 'me, Scrott, Jennifer',
+          subtitle: "Wish I could come, but I'm out of town this weekend.",
+        },
+        {
+          action: '6 hr',
+          headline: 'Oui oui',
+          title: 'Sandra Adams',
+          subtitle: 'Do you have Paris recommendations? Have you ever been?',
+        },
+        {
+          action: '12 hr',
+          headline: 'Birthday gift',
+          title: 'Trevor Hansen',
+          subtitle: 'Have any ideas about what we should get Heidi for her birthday?',
+        },
+        {
+          action: '18hr',
+          headline: 'Recipe to try',
+          title: 'Britta Holt',
+          subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+        },
+      ],
+    }),
+  }
+</script>
+<style media="screen">
+  .edit-btn {
+    position: fixed;
+    right: 30px;
+    bottom: 30px;
+  }
+</style>
