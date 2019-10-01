a20057db98a924324d5604221b83ea7e96194e60
aungwin <aungthiwin@sys-city.com>
2019-10-02 02:01:08 +0630

    modified vuerouter index.js

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,57 @@
 <template>
   <v-app>
-    <v-app-bar app>
+    <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <router-link
+        to='/'
+        tag='span'
+        style='cursor: pointer'>
+        Movie Man
+        </router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-flex xs12 sm6 md3>
+        <v-text-field
+          label='Movie Name'
+          v-model='searchString'
+          >
+        </v-text-field>
+        </v-flex>
       <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+        flat
+        :disabled="!dataAvailable"
+        @click="searchMovie"
       >
-        <span class="mr-2">Latest Release</span>
+       <span class="mr-2">Search</span>
       </v-btn>
-    </v-app-bar>
+    </v-toolbar>
 
     <v-content>
-      <Dashboard/>
+    <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld';
-import Dashboard from './components/Dashboard';
-
 export default {
   name: 'App',
   components: {
-    Dashboard,
   },
-  data: () => ({
-    //
-  }),
-};
+  data () {
+    return {
+      searchString: ''
+    }
+  },
+  methods: {
+    searchMovie () {
+      this.$router.push('/search/' + this.searchString)
+      this.searchString = ''
+    }
+  },
+  computed: {
+    dataAvailable () {
+      return this.searchString !== null && this.searchString !== ''
+    }
+  }
+}
 </script>
